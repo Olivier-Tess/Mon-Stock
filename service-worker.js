dce9d7a01bd190fa9e2d97db30b41f1ba1a8b669
Olivier-Tess <olivier.tess@free.fr>
2026-04-29 01:20:17 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,5 @@
-const CACHE_NAME = "monstock-v12";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-];
-
-self.addEventListener("install", event => {
-  self.skipWaiting();
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
-});
-
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(cacheNames => {
-    return Promise.all(cacheNames.map(cache => {
-      if (cache !== CACHE_NAME) return caches.delete(cache);
-    }));
-  }));
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
-});
+const CACHE_NAME = "monstock-v16";
+const urlsToCache = ["./", "./index.html", "./manifest.json", "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"];
+self.addEventListener("install", event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))); });
+self.addEventListener("activate", event => { event.waitUntil(caches.keys().then(names => Promise.all(names.map(n => { if (n !== CACHE_NAME) return caches.delete(n); })))); });
+self.addEventListener("fetch", event => { event.respondWith(caches.match(event.request).then(res => res || fetch(event.request))); });
